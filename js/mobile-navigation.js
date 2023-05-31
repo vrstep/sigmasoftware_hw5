0d005a052215687f82818f991dd37d0aef7dcfdb
Amir <amir.batyrggaliev@gmail.com>
2023-05-31 17:00:52 +0600

    Add shadow thing on mobile nav

--- js/mobile-navigation.js
+++ js/mobile-navigation.js
@@ -1,6 +1,9 @@
+const primaryHeader = document.querySelector('.primary-header');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
 navToggle.addEventListener('click', () => {
     primaryNav.toggleAttribute('data-visible');
+
+    primaryHeader.toggleAttribute('data-overlay');
 });
