4903f555169b669b6c54a873b5dcf48aa75f4a36
Amir <amir.batyrggaliev@gmail.com>
2023-06-05 18:39:44 +0600

    Add sticking header

---
+++ js/scrolling.js
@@ -0,0 +1,10 @@
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting);
+});
+
+navObserver.observe(scrollWatcher);
